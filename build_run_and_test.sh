8cdc1303185912452740e60ae99387d5d23fcff7
Семен Федотов <s.fedotov@MacBook-Pro-admin-22.local>
2018-12-16 20:48:38 +0300

    removed rm (:

--- build_run_and_test.sh
+++ build_run_and_test.sh
@@ -5,7 +5,7 @@ g++ -std=c++14 external_sort.cpp -o external_sort
 echo done!
 
 echo start randomizing file...
-python3 random_file_generator.py --lines-number 53335 --max-line-length 35 --output-filename generated_for_sort.txt
+python3 random_file_generator.py --lines-number 93335 --max-line-length 80 --output-filename generated_for_sort.txt
 echo done!
 
 echo start sort ...
@@ -18,7 +18,6 @@ echo start checking sorting order...
 
 python3 sorting_checker.py --input-filename generated_for_sort.txt
 
-rm generated_for_sort.txt
 rm external_sort
 
 echo end
